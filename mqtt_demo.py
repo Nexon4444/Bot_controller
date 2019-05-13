33f34496803f4f2b71e0473dd7a3c4501c60b7f2
Nexon4444 <maciek44444@gmail.com>
2019-05-14 01:54:29 +0200

    mqtt test

--- mqtt_demo.py
+++ mqtt_demo.py
@@ -1,7 +1,7 @@
 import paho.mqtt.publish as publish
 
 
-publish.single("Example/test", "Hello", hostname="192.168.0.110")
-publish.single("Example/topic", "World", hostname="192.168.0.110")
+publish.single("test", "Hello", hostname="192.168.0.106")
+# publish.single("Example/topic", "World", hostname="192.168.0.110")
 
-print("OK")
+# print("OK")
