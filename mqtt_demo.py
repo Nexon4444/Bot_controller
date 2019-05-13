5be8f1bef16a781d030f66f26b6ad63dd03ad2b9
Maciej Kutrowski <maciek44444@gmail.com>
2019-05-14 00:13:12 +0200

    changed to message

--- mqtt_demo.py
+++ mqtt_demo.py
@@ -1,7 +1,10 @@
-import paho.mqtt.publish as publish
+from paho.mqtt.client import Client
 
 
-publish.single("test", "Hello", hostname="192.168.0.106")
+client = Client("Publisher")
+client.connect(host="192.168.0.106", port=2000)
+client.publish("test", "test message")
+# publish.single("test", "Hello", hostname="192.168.0.106", port=2000)
 # publish.single("Example/topic", "World", hostname="192.168.0.110")
 
 # print("OK")
