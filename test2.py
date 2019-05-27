6eb82341ccfb3ca4353e372126b778dcbb0911ea
Maciej Kutrowski <maciej.michal.kutrowski@gmail.com>
2019-05-27 18:30:40 +0200

    MAchina liczaca

--- test2.py
+++ test2.py
@@ -1,11 +1,27 @@
 import paho.mqtt.client as mqtt
 import logging
-
+import time
 def on_message(client, userdata, msg):
     logging.debug(msg)
 
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        # client.connected_flag = True  # set flag
+        logging.debug("connected OK")
+    else:
+        logging.debug("Bad connection Returned code=", rc)
+
+
 client = mqtt.Client("client")
-client.connect("192.168.0.106")
-client.subscribe("2/receive")
 client.on_message = on_message
-client.loop_forever()
+client.on_connect = on_connect
+client.connect_async("192.168.0.106", port=2000)
+client.subscribe("2/receive")
+client.loop_start()
+
+client2 = mqtt.Client("client2")
+client2.on_connect = on_connect
+client2.on_message = on_message
+client2.connect_async("192.168.0.106", port=2000)
+client2.publish(topic="2/receive", payload="asdasdasdasd")
+# time.sleep(50)
